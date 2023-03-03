34066b0f23828972d60f86c081a853bd45a3442f
bna <skeletonpro1@mail.ru>
2023-03-03 16:57:39 +0500

    update address

--- src/components/Request.js
+++ src/components/Request.js
@@ -19,7 +19,7 @@ export const requestToApi = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + userDetails.tokenAccess}
             }
-        return fetch('http://localhost:8080' + url, {
+        return fetch('http://193.168.49.7:8080' + url, {
             method: 'POST',
             headers: header,
             body: JSON.stringify(body)
