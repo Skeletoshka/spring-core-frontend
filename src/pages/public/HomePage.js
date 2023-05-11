9b43a20baa68269121516a93837999db4596bd8a
bna <skeletonpro1@mail.ru>
2023-05-11 15:43:44 +0500

    bug fix

--- src/pages/public/HomePage.js
+++ src/pages/public/HomePage.js
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 
 export default function HomePage() {
     const navigate = useNavigate()
+    localStorage.setItem("tokenAccess", undefined)
     return (
         <div>
             <h1>Домашняя страница</h1>
