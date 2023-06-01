fb17a8d4f83eb1837eacd2e3801f741dee009229
bna <skeletonpro1@mail.ru>
2023-06-01 16:55:35 +0500

    Заявки + чистка

--- src/pages/public/HomePage.js
+++ src/pages/public/HomePage.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {Button, Menu} from "antd";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {requestToApi} from "../../components/Request";
-import {UserOutlined} from '@ant-design/icons'
-import NewsBlock from "../../components/NewsBlock";
 import NewsPublic from "./News";
 
 export default function HomePage() {
